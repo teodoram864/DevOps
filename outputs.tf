fa9bebce47a8a52ef44842184db38e1d9dcb6d22
Teodora Miteva <teodora.mtv@gmail.com>
2025-02-07 13:03:11 +0100

    V1

---
+++ outputs.tf
@@ -0,0 +1,18 @@
+
+output "vmfront_ip" {
+    value = azurerm_linux_virtual_machine.linuxvm1.public_ip_address
+}
+
+output "vmback_ip" {
+    value = azurerm_linux_virtual_machine.linuxvm2.private_ip_address
+}
+
+output "PrivateKeyFront" {
+    value = tls_private_key.ssh_1.private_key_pem
+    sensitive = true
+}
+
+output "PrivateKeyBack" {
+    value = tls_private_key.ssh_2.private_key_pem
+    sensitive = true
+}
---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "resource_group_name" {
+    type = string
+    description = "RG name in Azure"
+}
+
+variable  "resource_group_location" {
+  type        = string
+  description = "RG location in Azure"
+}
+
+variable "virtual_network_name" {
+  type        = string
+  description = "VNET name in Azure"
+}
+
+variable "subnet_name_1" {
+  type        = string
+  description = "Subnet for frontend inside the vNet"
+}
+
+variable "subnet_name_2" {
+  type        = string
+  description = "Subnet for backend inside the vNet"
+}
+
+variable "public_ip_name" {
+    type = string
+    description = "Public IP for frontend in Azure"
+}
+
+variable "network_security_group_name" {
+    type = string
+    description = "NSG name in Azure"
+}
+
+variable "network_interface_name_1" {
+    type = string
+    description = "NIC name for front in Azure"
+}
+
+variable "network_interface_name_2" {
+    type = string
+    description = "NIC name for backend Azure"
+}
+
+variable "linux_virtual_machine_name_1" {
+    type = string
+    description = "Linux VM frontend in Azure"
+}
+variable "linux_virtual_machine_name_2" {
+    type = string
+    description = "Linux VM backend in Azure"
+}
+variable tls_private_key_1 {
+  type        = string
+  description = "Private key for linuxvmfront"
+}
+variable tls_private_key_2 {
+  type        = string
+  description = "Private key for linuxvmback"
+}
+
+
+    
+
+
+
+
+
