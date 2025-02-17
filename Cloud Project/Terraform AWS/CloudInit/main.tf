b2062a1a3fc94563f1b709bf660b445b1d2b3a7b
HaAymar <he201905@students.ephec.be>
2025-02-17 09:51:14 +0100

    Adding cloud-init module for vm1

---
+++ Cloud Project/Terraform AWS/CloudInit/main.tf
@@ -0,0 +1,77 @@
+# DATA
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "allow_ssh_http_80_cloudInit" {
+  name        = "allow_ssh_http_cloudInit"
+  description = "Allow SSH on port 22 & HTTP on port 80"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "server_group03" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http_80_cloudInit.id]
+  subnet_id              = tolist(data.aws_subnets.default.ids)[0]
+
+  user_data = file("./cloud-init.yaml")
+
+  tags = {
+    Name = "server_group03"
+  }
+}
---
+++ Cloud Project/Terraform AWS/CloudInit/outputs.tf
@@ -0,0 +1,3 @@
+output "aws_instance_public_dns" {
+  value = aws_instance.server_group03.public_dns
+}
